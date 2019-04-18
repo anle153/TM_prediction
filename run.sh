9805f37d2a7519004a1790104cf1ecffb7671e03
anle153 <anle@nii.ac.jp>
2019-04-18 10:49:25 +0900

    Add: create abilene 3d

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #/usr/bin/bash
 
-python run.py --run_mode=training --alg=fwbw-conv-lstm --tag=1.0 --data_name=Abilene --gpu=1
+python run.py --run_mode=training --alg=fwbw-conv-lstm --tag=1.0 --data_name=Abilene --gpu=0
 

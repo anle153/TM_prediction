a94369dd0198b2e33e7c5b3d3121701497bc9442
anle153 <anle@nii.ac.jp>
2019-04-21 19:11:10 +0900

    Fix fwbw testing

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-python run.py --run_mode=train --alg=fwbw-conv-lstm --tag=mon30_lstm26 --data_name=Abilene2d
+python run.py --run_mode=train --alg=fwbw-conv-lstm --tag=mon30_lstm26 --data_name=Abilene

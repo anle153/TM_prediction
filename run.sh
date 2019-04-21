07fecbf0dd09bf89bbce678567073762fed272c9
anle153 <anle@nii.ac.jp>
2019-04-21 21:41:14 +0900

    train arima Geant

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-python run.py --run_mode=train --alg=lstm-nn --tag=mon_30_lstm_288 --data_name=Abilene2d
+python run.py --run_mode=train --alg=arima --tag=mon_30_lstm_288 --data_name=Geant2d

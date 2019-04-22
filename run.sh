a36847ef5d1cd4f7b542476e188b7edf741f9f7a
anle153 <anle@nii.ac.jp>
2019-04-22 09:49:27 +0900

    lstm test

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-python run.py --run_mode=train --alg=arima --tag=mon_30_lstm_288 --data_name=Geant2d
+python run.py --run_mode=test --alg=lstm --tag=mon_30_lstm_288 --data_name=Abilene2d

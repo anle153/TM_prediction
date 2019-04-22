5455189258d022190777596a389695c64ea94911
anle153 <anle@nii.ac.jp>
2019-04-22 12:19:52 +0900

    lstm-nn test

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-python run.py --run_mode=test --alg=arima --tag=mon_30_lstm_288 --data_name=Geant2d
+python run.py --run_mode=test --alg=lstm-nn --tag=mon_30_lstm_288 --data_name=Abilene2d

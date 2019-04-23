b421d0dd56d7ebc65a8f8bc55c314c78f3d22aaa
anle153 <anle@nii.ac.jp>
2019-04-23 11:25:23 +0900

    arima test update 7w

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-python run.py --run_mode=test --alg=lstm-nn --tag=mon_30_lstm_288 --data_name=Abilene2d --gpu=1
+python run.py --run_mode=test --alg=arima --tag=mon_30_update_7w --data_name=Abilene2d --gpu=1

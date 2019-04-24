f9e13202c3701621b0210a0c4dce6d8d0cc86e41
anle153 <anle@nii.ac.jp>
2019-04-24 12:19:39 +0900

    test arima 60d

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-python run.py --run_mode=test --alg=holt-winter --tag=mon_30_update_7d_test_30d --data_name=Abilene2d --gpu=0
+python run.py --run_mode=test --alg=arima --tag=mon_30_update_7d_test_60d --data_name=Abilene2d --gpu=0

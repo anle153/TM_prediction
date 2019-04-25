bd2214c0785629e8e7708dbd232a2378757f614a
anle153 <anle@nii.ac.jp>
2019-04-25 11:57:51 +0900

    arima geant all 10

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-python run.py --run_mode=test --alg=arima --tag=mon_10_update_7d --data_name=Abilene2d --gpu=1
+python run.py --run_mode=test --alg=arima --tag=mon_10_update_7d --data_name=Geant2d --gpu=1

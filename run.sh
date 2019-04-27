76ce24bc911a3048dd0cc29dcf643f26fa4c7af1
anle153 <anle@nii.ac.jp>
2019-04-27 14:27:15 +0900

    train lstm-nn_deep3

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-python run.py --run_mode=train --alg=lstm-nn --tag=mon_30_lstm_26_deep_3 --data_name=Abilene2d --gpu=1
+python run.py --run_mode=train --alg=lstm-nn --tag=mon_30_lstm_26_deep_3 --data_name=Abilene2d --gpu=0

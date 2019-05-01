99f4159c1c0c00990a39d1082f4ff74c40eab6ee
anle153 <anle@nii.ac.jp>
2019-05-01 22:16:11 +0900

    train lstm-nn Geant 30 10

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-python run.py --run_mode=train --alg=fwbw-conv-lstm --tag=mon_30_lstm_26 --data_name=Abilene --gpu=1
+python run.py --run_mode=test --alg=conv-lstm --tag=mon_30_lstm_26 --data_name=Abilene --gpu=0

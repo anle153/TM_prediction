1ab92198bbab2084cb3002e3804ed6a7d6f12661
anle153 <anle@nii.ac.jp>
2019-05-01 18:23:09 +0900

    test fwbw Geant 30 10

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-python run.py --run_mode=test --alg=fwbw-conv-lstm --tag=mon_30_lstm_10 --data_name=Geant --gpu=1
+python run.py --run_mode=plot --alg=fwbw-conv-lstm --tag=mon_30_lstm_10 --data_name=Geant --gpu=1

3a908a703befea45b1f09b2f35c07c338331768b
anle153 <anle@nii.ac.jp>
2019-05-04 13:23:22 +0900

    train lstm-nn with minmax scaler

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-python run.py --run_mode=plot --alg=fwbw-conv-lstm --tag=mon_30_lstm_26_mixmax --data_name=Abilene --gpu=1
+python run.py --run_mode=test --alg=fwbw-conv-lstm --tag=mon_30_lstm_26_minmax --data_name=Abilene --gpu=1

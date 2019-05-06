c2fdcdad5e21e2209b6e5335f96546537250d773
anle153 <anle@nii.ac.jp>
2019-05-06 23:02:43 +0900

    train lstm robust

--- common/Config.py
+++ common/Config.py
@@ -12,7 +12,7 @@ ALL_DATA = True
 NUM_DAYS = 160
 
 SCALERS = ['power-transform', 'standard-scaler', 'minmax-scaler', 'box-cox', 'robust-scaler']
-SCALER = SCALERS[4]
+SCALER = SCALERS[2]
 # ----------------------------------------------
 
 # ----------------- LSTM Config ---------------
@@ -52,7 +52,7 @@ FWBW_CONV_LSTM_WIDE = 12
 FWBW_CONV_LSTM_HIGH = 12
 FWBW_CONV_LSTM_CHANNEL = 2
 
-FWBW_CONV_LSTM_MON_RAIO = 0.3
+FWBW_CONV_LSTM_MON_RAIO = 0.4
 
 FWBW_CONV_LSTM_IMS_STEP = 4
 FWBW_CONV_LSTM_STEP = 26

5493e485ae312eb6b7beaed4892bb4126357f709
anle153 <anle@nii.ac.jp>
2019-05-07 22:55:24 +0900

    train fwbwlstm nn 0.55 minmax

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-python run.py --run_mode=train --alg=fwbw-conv-lstm --tag=mon_25_lstm_26 --data_name=Abilene2d --gpu=1
+python run.py --run_mode=train --alg=fwbw-conv-lstm --tag=mon_35_lstm_26 --data_name=Abilene2d --gpu=1

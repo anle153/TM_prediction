76035b741c51df7bb3d87bf9ec09bf0677c2becf
An Le <anle@nii.ac.jp>
2019-05-08 20:54:28 +0700

    commit

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #/usr/bin/bash
 
-<<<<<<< HEAD
 python run.py --run_mode=train --alg=xgb --tag=mon_30_lstm_26 --data_name=Abilene2d --gpu=0
-=======
-python run.py --run_mode=train  --alg=fwbw-conv-lstm --tag=mon_30_lstm_26 --data_name=Abilene2d --gpu=1
->>>>>>> 96fb09721138396a4182101d66d1ea614bf7dfb2

83514c34a2675a8694fda9c699ef2914ae154913
anle153 <anle@nii.ac.jp>
2019-05-09 14:01:09 +0900

    fix args

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-import sys
-
 import numpy as np
 from comet_ml import Experiment
 
@@ -101,4 +99,4 @@ def main():
 if __name__ == '__main__':
     # create_Geant2d(save_csv=True)
     # create_abilene_data_2d(path='/home/anle/AbileneTM-all/', save_csv=True)
-    main(sys.argv)
+    main()

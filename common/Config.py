ae0dc66eec912556587693ea3ada588436f4441b
anle153 <anle@nii.ac.jp>
2019-05-09 23:03:17 +0900

    fix args

--- common/Config.py
+++ common/Config.py
@@ -130,7 +130,7 @@ ALGS = ['fwbw-conv-lstm', 'conv-lstm', 'lstm-nn', 'arima', 'holt-winter', 'xgb']
 
 DATA_NAME = 'Abilene2d'
 
-RUN_MODE = RUN_MODES[1]
+RUN_MODE = RUN_MODES[0]
 ALG = ALGS[0]
 GPU = 1
 

b2ec3d6d78ea2488dfc71a6566edd150449bb71a
anle153 <anle@nii.ac.jp>
2019-05-11 18:33:12 +0900

    fix cinv-lstm

--- common/Config.py
+++ common/Config.py
@@ -117,7 +117,7 @@ SCALERS = ['power-transform', 'standard-scaler', 'minmax-scaler', 'box-cox', 'ro
 DATA_NAME = 'Abilene2d'
 
 RUN_MODE = RUN_MODES[0]
-ALG = ALGS[2]
+ALG = ALGS[1]
 GPU = 0
 SCALER = SCALERS[2]
 

db7785dfad72dbbd829a630f60285f7a3c8ebb0b
anle <anle@nii.ac.jp>
2019-05-24 02:39:36 +0900

    commit

--- Models/fwbw_LSTM.py
+++ Models/fwbw_LSTM.py
@@ -57,7 +57,7 @@ class fwbw_lstm_model(AbstractModel):
         # fc_2 = Dense(32, )(fc_1)
         # fc_3 = Dense(24, name='correct_data')(fc_2)
 
-        input_tensor_flatten = tf.reshape(input_tensor.output, shape=[-1])
+        input_tensor_flatten = tf.keras.layers.Reshape((self.input_shape[0]*self.input_shape[1]),input_shape=self.input_shape)
         _input = concatenate(inputs=[input_tensor_flatten, fw_outputs, bw_outputs])
 
         x = Dense(64,)(_input)

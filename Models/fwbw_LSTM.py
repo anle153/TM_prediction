3ee8bcb53281cb1b4893a3fe83a2ddc7bbaaafdd
anle153 <anle@nii.ac.jp>
2019-05-23 19:45:40 +0900

    commit

--- Models/fwbw_LSTM.py
+++ Models/fwbw_LSTM.py
@@ -51,21 +51,13 @@ class fwbw_lstm_model(AbstractModel):
         _input = input_tensor[:, 1:-1, 0]
         _labels = input_tensor[:, 1:-1, 1]
 
-        _in_tensor = tf.constant(0, shape=[0, 24 * 4])
-        tf.assign(_in_tensor[:, 0:24], _fw_outs)
-        tf.assign(_in_tensor[:, 24:48], _bw_outs)
-        tf.assign(_in_tensor[:, 48:72], _input)
-        tf.assign(_in_tensor[:, 72:], _labels)
-
+        _in_tensor = tf.concat([_fw_outs, _bw_outs, _input, _labels], axis=1)
 
         fc_1 = Dense(64, )(_in_tensor)
         fc_2 = Dense(32, )(fc_1)
         fc_3 = Dense(24, )(fc_2)
 
-        outputs = tf.constant(0, shape=[0, 26])
-        outputs[:, 0] = input_tensor[:, 0, 0]
-        outputs[:, 1:-1] = fc_3[:, :, 0]
-        outputs[:, -1] = fw_outputs[:, -1, 0]
+        outputs = tf.concat([input_tensor[:, 0, 0], fc_3[:, :, 0], fw_outputs[:, -1, 0]], axis=1)
 
         self.model = Model(inputs=input_tensor, outputs=outputs, name='fwbw-lstm')
 

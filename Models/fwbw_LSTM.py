e19afb3ce159f76988dd442560862498f7f42c3b
anle153 <anle@nii.ac.jp>
2019-05-24 13:09:31 +0900

    commit

--- Models/fwbw_LSTM.py
+++ Models/fwbw_LSTM.py
@@ -26,9 +26,11 @@ class fwbw_lstm_model(AbstractModel):
         fw_dense_2 = TimeDistributed(Dense(32, ))(fw_dense_1)
         fw_outputs = TimeDistributed(Dense(1, name='fw_output'))(fw_dense_2)
 
-        # self.fw_model = Model(inputs=input_tensor, outputs=fw_outputs, name='FW_Model')
-        #
-        # self.fw_model.compile(loss='mse', optimizer='adam', metrics=['mse', 'mae'])
+        fw_out = Flatten()(fw_outputs)
+        fw_out = Dense(256, )(fw_out)
+        fw_out = Dense(128, )(fw_out)
+        fw_out = Dense(64, )(fw_out)
+        fw_out = Dense(1, )(fw_out)
 
         bw_lstm_layer = LSTM(self.hidden, input_shape=self.input_shape,
                              return_sequences=True, go_backwards=True)(input_tensor)
@@ -40,22 +42,6 @@ class fwbw_lstm_model(AbstractModel):
         bw_dense_2 = TimeDistributed(Dense(32, ))(bw_dense_1)
         bw_outputs = TimeDistributed(Dense(1, ))(bw_dense_2)
 
-        # self.bw_model = Model(inputs=input_tensor, outputs=bw_outputs, name='BW_Model')
-        #
-        # self.bw_model.compile(loss='mse', optimizer='adam', metrics=['mse', 'mae'])
-
-        # _fw_outs = fw_outputs[:, :-2, 0]
-        # dims = [1]
-        # _bw_outs = tf.reverse(bw_outputs, dims)[:, 2:, 0]
-        # _input = input_tensor[:, 1:-1, 0]
-        # _labels = input_tensor[:, 1:-1, 1]
-
-        # _in_tensor = tf.concat([_fw_outs, _bw_outs, _input, _labels], axis=1)
-        #
-        # fc_1 = Dense(64, )(_in_tensor)
-        # fc_2 = Dense(32, )(fc_1)
-        # fc_3 = Dense(24, name='correct_data')(fc_2)
-
         input_tensor_flatten = Reshape((self.input_shape[0] * self.input_shape[1], 1))(input_tensor)
         _input = Concatenate(axis=1)([input_tensor_flatten, fw_outputs, bw_outputs])
 
@@ -63,6 +49,6 @@ class fwbw_lstm_model(AbstractModel):
         x = Dense(64, )(x)
         outputs = Dense(24,)(x)
 
-        self.model = Model(inputs=input_tensor, outputs=[outputs, fw_outputs], name='fwbw-lstm')
+        self.model = Model(inputs=input_tensor, outputs=[outputs, fw_out], name='fwbw-lstm')
 
         self.model.compile(loss='mse', optimizer='adam', metrics=['mse', 'mae'], loss_weights=[1., 0.2])

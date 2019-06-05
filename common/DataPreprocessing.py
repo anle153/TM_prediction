55301d68c4642421f3bd497be1989c18207ce0d9
anle153 <anle@nii.ac.jp>
2019-06-05 18:07:56 +0900

    commit

--- common/DataPreprocessing.py
+++ common/DataPreprocessing.py
@@ -75,10 +75,11 @@ def create_offline_fwbw_conv_lstm_data_fix_ratio(data, input_shape, mon_ratio, e
     wide = input_shape[1]
     high = input_shape[2]
     channel = input_shape[3]
-    dataX = np.zeros(
-        ((data.shape[0] - ntimesteps - 1) * data_time, ntimesteps, wide, high, channel))
-    dataY_1 = np.zeros(((data.shape[0] - ntimesteps - 1) * data_time, ntimesteps, wide * high))
-    dataY_2 = np.zeros(((data.shape[0] - ntimesteps - 1) * data_time, ntimesteps, wide * high))
+    dataX = np.zeros(((data.shape[0] - ntimesteps) * data_time, ntimesteps, wide, high, channel))
+    dataY_1 = np.zeros(((data.shape[0] - ntimesteps) * data_time, ntimesteps, wide * high))
+    dataY_2 = np.zeros(((data.shape[0] - ntimesteps) * data_time, ntimesteps, wide * high))
+
+    print(dataX.shape)
 
     for time in range(data_time):
         _labels = np.random.choice(_tf,
@@ -92,10 +93,10 @@ def create_offline_fwbw_conv_lstm_data_fix_ratio(data, input_shape, mon_ratio, e
         _traffic_labels[:, :, :, 0] = _data
         _traffic_labels[:, :, :, 1] = _labels
 
-        for idx in range(1, _traffic_labels.shape[0] - ntimesteps, 1):
+        for idx in range(_traffic_labels.shape[0] - ntimesteps):
             _x = _traffic_labels[idx: (idx + ntimesteps)]
 
-            dataX[idx - 1 + time * (data.shape[0] - ntimesteps)] = _x
+            dataX[idx + time * (data.shape[0] - ntimesteps)] = _x
 
             _y = data[(idx + 1):(idx + ntimesteps + 1)]
             _y = np.reshape(_y, newshape=(ntimesteps, wide * high))
@@ -103,9 +104,9 @@ def create_offline_fwbw_conv_lstm_data_fix_ratio(data, input_shape, mon_ratio, e
             _y_2 = data[(idx - 1):(idx + ntimesteps - 1)]
             _y_2 = np.reshape(np.flip(_y_2, axis=0), newshape=(ntimesteps, wide * high))
 
-            dataY_1[idx - 1 + time * (data.shape[0] - ntimesteps)] = _y
+            dataY_1[idx + time * (data.shape[0] - ntimesteps)] = _y
 
-            dataY_2[idx - 1 + time * (data.shape[0] - ntimesteps)] = _y_2
+            dataY_2[idx + time * (data.shape[0] - ntimesteps)] = _y_2
 
     return dataX, dataY_1, dataY_2
 

29d3d29d0e4e7b5d8ae3b925dbac3481eace6d03
anle153 <anle@nii.ac.jp>
2019-06-10 10:41:18 +0900

    commit

--- common/Config.py
+++ common/Config.py
@@ -208,7 +208,7 @@ SCALERS = ['power-transform', 'standard-scaler', 'minmax-scaler', 'box-cox', 'ro
 
 DATA_NAME = 'Abilene2d'
 
-RUN_MODE = RUN_MODES[1]
+RUN_MODE = RUN_MODES[0]
 ALG = ALGS[0]
 GPU = 0
 SCALER = SCALERS[5]

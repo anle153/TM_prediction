5bfa5dc9f5bf6664d1859d28f44c2de905866bea
anle153 <anle@nii.ac.jp>
2019-06-19 00:50:23 +0900

    commit

--- common/Config_res_lstm_2.py
+++ common/Config_res_lstm_2.py
@@ -12,7 +12,7 @@ RES_LSTM_2_IMS_STEP = 12
 
 RES_LSTM_2_MON_RAIO = 0.30
 
-RES_LSTM_2_BEST_CHECKPOINT = 5
+RES_LSTM_2_BEST_CHECKPOINT = 14
 RES_LSTM_2_TESTING_TIME = 10
 
 RES_LSTM_2_IMS = False
@@ -38,7 +38,7 @@ DATA_SETS = ['Abilene2d', 'Geant2d']
 DATA_NAME = DATA_SETS[0]
 RUN_MODE = RUN_MODES[0]
 ALG = 'res-lstm-2'
-GPU = 0
+GPU = 1
 SCALER = SCALERS[5]
 
 TAG = 'mon_{:02d}_lstm_{:02d}_batch_{:03d}_hidden_{:03d}'.format(int(RES_LSTM_2_MON_RAIO * 100),

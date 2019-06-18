63d2165c9ed80ca7aa1b744a5c7a8f35ceba4991
anle153 <anle@nii.ac.jp>
2019-06-19 04:34:30 +0900

    commit

--- common/Config_fwbw_lstm.py
+++ common/Config_fwbw_lstm.py
@@ -31,7 +31,7 @@ SCALERS = ['power-transform', 'standard-scaler', 'minmax-scaler', 'box-cox', 'ro
 DATA_SETS = ['Abilene2d', 'Geant2d']
 
 DATA_NAME = DATA_SETS[0]
-RUN_MODE = RUN_MODES[1]
+RUN_MODE = RUN_MODES[0]
 ALG = 'fwbw-lstm'
 GPU = 1
 SCALER = SCALERS[5]
--- run_fwbw_lstm.py
+++ run_fwbw_lstm.py
@@ -24,9 +24,7 @@ def print_fwbw_lstm_info():
         print('|--- LSTM_DEEP_NLAYERS:\t{}'.format(Config.FWBW_LSTM_DEEP_NLAYERS))
     print('|--- LSTM_DROPOUT:\t{}'.format(Config.FWBW_LSTM_DROPOUT))
     print('|--- LSTM_HIDDEN_UNIT:\t{}'.format(Config.FWBW_LSTM_HIDDEN_UNIT))
-    print('|--- RANDOM_ACTION:\t{}'.format(Config.FWBW_LSTM_RANDOM_ACTION))
-    if not Config.FWBW_LSTM_RANDOM_ACTION:
-        print('|--- FWBW_LSTM_HYPERPARAMS:\t{}'.format(Config.FWBW_LSTM_HYPERPARAMS))
+    print('|--- FLOW_SELECTION:\t{}'.format(Config.FWBW_LSTM_FLOW_SELECTION))
 
     if Config.RUN_MODE == Config.RUN_MODES[0]:
         print('|--- N_EPOCH:\t{}'.format(Config.FWBW_LSTM_N_EPOCH))

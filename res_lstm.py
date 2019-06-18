d78b7384f4316dcd856220919ac679cb973f5e7b
anle153 <anle@nii.ac.jp>
2019-06-18 15:12:25 +0900

    commit

--- res_lstm.py
+++ res_lstm.py
@@ -3,6 +3,6 @@ import numpy as np
 from algs.res_fwbw_lstm import train_res_fwbw_lstm
 from common import Config
 
-if __name__ == '__main':
+if __name__ == '__main__':
     data = np.load(Config.DATA_PATH + '{}.npy'.format(Config.DATA_NAME))
     train_res_fwbw_lstm(data)

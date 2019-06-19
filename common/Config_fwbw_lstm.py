e78789fdcb8d1594b7381dc20ef9795b68e27056
anle153 <anle@nii.ac.jp>
2019-06-19 16:42:50 +0900

    commit

--- common/Config_fwbw_lstm.py
+++ common/Config_fwbw_lstm.py
@@ -12,7 +12,7 @@ FWBW_LSTM_STEP = 30
 FWBW_LSTM_FEATURES = 2
 FWBW_LSTM_IMS_STEP = 12
 
-FWBW_LSTM_MON_RAIO = 0.50
+FWBW_LSTM_MON_RAIO = 0.60
 
 FWBW_LSTM_BEST_CHECKPOINT = 5
 
@@ -39,7 +39,7 @@ DATA_SETS = ['Abilene2d', 'Geant2d']
 DATA_NAME = DATA_SETS[0]
 RUN_MODE = RUN_MODES[0]
 ALG = 'fwbw-lstm'
-GPU = 0
+GPU = 1
 SCALER = SCALERS[5]
 
 # --------------- Data Config -----------------

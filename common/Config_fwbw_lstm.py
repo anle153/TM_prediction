315595aaf1fbaac4ff499202c8c1fe249e78f464
anle153 <anle@nii.ac.jp>
2019-06-19 13:23:49 +0900

    commit

--- common/Config_fwbw_lstm.py
+++ common/Config_fwbw_lstm.py
@@ -33,7 +33,7 @@ DATA_SETS = ['Abilene2d', 'Geant2d']
 DATA_NAME = DATA_SETS[0]
 RUN_MODE = RUN_MODES[1]
 ALG = 'fwbw-lstm'
-GPU = 1
+GPU = 0
 SCALER = SCALERS[5]
 
 # --------------- Data Config -----------------

506a3cc45236e457c3405bca2265e68971d832bd
anle153 <anle@nii.ac.jp>
2019-06-20 17:30:49 +0900

    commit

--- common/Config_lstm.py
+++ common/Config_lstm.py
@@ -12,7 +12,7 @@ LSTM_STEP = 30
 LSTM_FEATURES = 2
 LSTM_IMS_STEP = 12
 
-LSTM_MON_RATIO = 0.80
+LSTM_MON_RATIO = 0.90
 
 LSTM_BEST_CHECKPOINT = 3
 
@@ -47,7 +47,7 @@ DATA_SETS = ['Abilene2d', 'Geant2d']
 DATA_NAME = DATA_SETS[0]
 RUN_MODE = RUN_MODES[0]
 ALG = 'lstm-nn'
-GPU = 1
+GPU = 0
 SCALER = SCALERS[5]
 
 TAG = 'mon_{:02d}_lstm_{:02d}_batch_{:03d}_hidden_{:03d}'.format(int(LSTM_MON_RATIO * 100),

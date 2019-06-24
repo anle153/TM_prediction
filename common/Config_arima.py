db4d9e06a8b16e8190b0a469be869c4fda0cdbe5
anle153 <anle@nii.ac.jp>
2019-06-24 09:38:13 +0900

    commit

--- common/Config_arima.py
+++ common/Config_arima.py
@@ -1,7 +1,7 @@
 # ----------- ARIMA Config ----------------------
 ARIMA_UPDATE = 7
 ARIMA_TESTING_TIME = 5
-ARIMA_MON_RATIO = 0.10
+ARIMA_MON_RATIO = 0.40
 ARIMA_STEP = 144
 ARIMA_IMS_STEP = 12
 ARIMA_IMS = False

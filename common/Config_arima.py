551c8eb788c471fa711b8a00eadf10a0ca3b1693
anle153 <anle@nii.ac.jp>
2019-07-21 20:27:23 +0900

    commit

--- common/Config_arima.py
+++ common/Config_arima.py
@@ -5,18 +5,18 @@ ARIMA_MON_RATIO = 0.40
 ARIMA_STEP = 36
 ARIMA_IMS_STEP = 3
 ARIMA_IMS = False
-ARIMA_TEST_DAYS = 5
+ARIMA_TEST_DAYS = 0.1
 # ----------------------------------------------
 
 
 RUN_MODES = ['train', 'test', 'plot']
-SCALERS = ['power-transform', 'standard-scaler', 'minmax-scaler', 'box-cox', 'robust-scaler', 'sd_scaler']
+SCALERS = ['power-transform']
 DATA_SETS = ['Abilene2d', 'Geant2d']
 
 DATA_NAME = DATA_SETS[0]
 RUN_MODE = RUN_MODES[1]
 ALG = 'arima'
-GPU = 1
+GPU = 0
 SCALER = SCALERS[0]
 
 # --------------- Data Config -----------------

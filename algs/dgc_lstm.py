84528ad2ae370612372f2857d9eef34643836068
anle153 <anle@nii.ac.jp>
2019-07-23 00:37:54 +0900

    commit

--- algs/dgc_lstm.py
+++ algs/dgc_lstm.py
@@ -3,7 +3,6 @@ import os
 import numpy as np
 import tensorflow as tf
 import yaml
-from sklearn.preprocessing import PowerTransformer
 from tqdm import tqdm
 
 from Models.dcrnn_supervisor import DCRNNRegressor
@@ -20,24 +19,24 @@ CONFIG_FILE = 'config_dgclstm.yaml'
 RESULTS_PATH = os.path.join(HOME_PATH, 'results')
 
 
-def create_data(data, step_in, step_out, input_dim, mon_ratio, eps):
+def create_data(data, seq_len, horizon, input_dim, mon_ratio, eps):
     _tf = np.array([1.0, 0.0])
     _labels = np.random.choice(_tf, size=data.shape, p=(mon_ratio, 1 - mon_ratio))
     _data = np.copy(data)
 
     _data[_labels == 0.0] = np.random.uniform(_data[_labels == 0.0] - eps, _data[_labels == 0.0] + eps)
 
-    x = np.zeros(shape=(data.shape[0] - step_in - step_out, step_in, data.shape[1], input_dim))
-    y = np.zeros(shape=(data.shape[0] - step_in - step_out, step_out, data.shape[1], 1))
+    x = np.zeros(shape=(data.shape[0] - seq_len - horizon, seq_len, data.shape[1], input_dim))
+    y = np.zeros(shape=(data.shape[0] - seq_len - horizon, horizon, data.shape[1], 1))
 
-    for idx in range(_data.shape[0] - step_in - step_out):
-        _x = _data[idx: (idx + step_in)]
-        _label = _labels[idx: (idx + step_in)]
+    for idx in range(_data.shape[0] - seq_len - horizon):
+        _x = _data[idx: (idx + seq_len)]
+        _label = _labels[idx: (idx + seq_len)]
 
         x[idx, :, 0] = _x
         x[idx, :, 1] = _label
 
-        _y = _data[idx + step_in:idx + step_in + step_out]
+        _y = _data[idx + seq_len:idx + seq_len + horizon]
 
         y[idx] = np.expand_dims(_y, axis=2)
 
@@ -64,26 +63,27 @@ def generate_data(config):
     else:
         day_size = config['geant_day_size']
 
-    step_in = config['step_in']
-    step_out = config['step_out']
+    seq_len = config['model']['seq_len']
+    horizon = config['model']['horizon']
+    input_dim = config['model']['input_dim']
 
-    mon_ratio = config['mon_ratio']
+    mon_ratio = config['train']['mon_ratio']
 
     print('|--- Splitting train-test set.')
     train_data2d, valid_data2d, test_data2d = prepare_train_valid_test_2d(data=data, day_size=day_size)
-    print('|--- Normalizing the train set.')
-    scaler = PowerTransformer()
-    scaler.fit(train_data2d)
-    train_data2d_norm = scaler.transform(train_data2d)
-    valid_data2d_norm = scaler.transform(valid_data2d)
-    test_data2d_norm = scaler.transform(test_data2d)
+    # print('|--- Normalizing the train set.')
+    # scaler = PowerTransformer()
+    # scaler.fit(train_data2d)
+    # train_data2d_norm = scaler.transform(train_data2d)
+    # valid_data2d_norm = scaler.transform(valid_data2d)
+    # test_data2d_norm = scaler.transform(test_data2d)
 
-    x_train, y_train = create_data(train_data2d_norm, step_in=step_in, step_out=step_out, input_dim=2,
-                                   mon_ratio=mon_ratio, eps=train_data2d_norm.mean)
-    x_val, y_val = create_data(valid_data2d_norm, step_in=step_in, step_out=step_out, input_dim=2,
-                               mon_ratio=mon_ratio, eps=train_data2d_norm.mean)
-    x_test, y_test = create_data(test_data2d_norm, step_in=step_in, step_out=step_out, input_dim=2,
-                                 mon_ratio=mon_ratio, eps=train_data2d_norm.mean)
+    x_train, y_train = create_data(train_data2d, seq_len=seq_len, horizon=horizon, input_dim=input_dim,
+                                   mon_ratio=mon_ratio, eps=train_data2d.mean)
+    x_val, y_val = create_data(valid_data2d, seq_len=seq_len, horizon=horizon, input_dim=input_dim,
+                               mon_ratio=mon_ratio, eps=train_data2d.mean)
+    x_test, y_test = create_data(test_data2d, seq_len=seq_len, horizon=horizon, input_dim=input_dim,
+                                 mon_ratio=mon_ratio, eps=train_data2d.mean)
 
     for cat in ["train", "val", "test"]:
         _x, _y = locals()["x_" + cat], locals()["y_" + cat]
@@ -94,7 +94,7 @@ def generate_data(config):
             y=_y,
         )
 
-    adj_mx = get_corr_matrix(train_data2d, step_in)
+    adj_mx = get_corr_matrix(train_data2d, seq_len)
     np.save(os.path.join(config['data']['adj_mx_path'], "{}.npz".format('Corr_matrix')),
             adj_mx)
 
@@ -108,7 +108,7 @@ def train_dgc_lstm():
     tf_config = tf.ConfigProto()
     tf_config.gpu_options.allow_growth = True
 
-    adj_mx = np.load(os.path.join(config['data']['adj_mx_path'], "{}.npz".format('Corr_matrix')))
+    adj_mx = np.load(os.path.join(config['data']['graph_pkl_filename'], "{}.npz".format('Corr_matrix')))
 
     with tf.Session(config=tf_config) as sess:
         model = DCRNNRegressor(adj_mx=adj_mx, **config)
@@ -124,7 +124,7 @@ def test_dgc_lstm():
     tf_config = tf.ConfigProto()
     tf_config.gpu_options.allow_growth = True
 
-    adj_mx = np.load(os.path.join(config['data']['adj_mx_path'], "{}.npz".format('Corr_matrix')))
+    adj_mx = np.load(os.path.join(config['data']['graph_pkl_filename'], "{}.npz".format('Corr_matrix')))
 
     with tf.Session(config=tf_config) as sess:
         model = DCRNNRegressor(adj_mx=adj_mx, **config)
@@ -135,4 +135,4 @@ def test_dgc_lstm():
                             **outputs)
 
         print('Predictions saved as {}.'.format(os.path.join(config['test']['results_path'],
-                                                             config['test']['results_name'])))
+                                                             config['test']['results_name'] + '.npz')))

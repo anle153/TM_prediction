e76b48a213f764c80687930de4ed5b35dc9b73af
anle153 <anle@nii.ac.jp>
2019-07-23 22:38:04 +0900

    commit

--- algs/dgc_lstm.py
+++ algs/dgc_lstm.py
@@ -121,10 +121,10 @@ def train_dgc_lstm(config):
 
     adj_mx = np.load(config['data']['graph_pkl_filename'] + '.npy')
     adj_mx = adj_mx.astype('float32')
+    with tf.device('/device:GPU:1'):
+        model = DCRNNSupervisor(adj_mx=adj_mx, **config)
     with tf.Session(config=tf_config) as sess:
-        with tf.device('/device:GPU:1'):
-            model = DCRNNSupervisor(adj_mx=adj_mx, **config)
-            model.train(sess)
+        model.train(sess)
 
 
 def test_dgc_lstm():

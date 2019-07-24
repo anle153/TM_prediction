7dc042bcb8c12c7eee358f917e30a49d18db1102
anle <anle@nii.ac.jp>
2019-07-24 09:03:06 +0900

    commit

--- eda.py
+++ eda.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 import yaml
 from common.DataPreprocessing import prepare_train_valid_test_2d
-
+from tqdm import tqdm
 
 
 HOME_PATH = os.path.expanduser('~/TM_prediction')
@@ -75,8 +75,82 @@ std_train = x_train.std()
 
 x_train_norm = (x_train - mean_train) / std_train
 y_train_norm = (y_train - mean_train) / std_train
+x_val_norm = (x_val - mean_train) / std_train
+y_val_norm = (y_val - mean_train) / std_train
+x_test_norm = (x_test - mean_train) / std_train
+y_test_norm = (y_test - mean_train) / std_train
 
 if np.any(np.isinf(x_train_norm)):
     print("INF")
+    raise ValueError
+
 if np.any(np.isnan(x_train_norm)):
     print("NAN")
+    raise ValueError
+
+if np.any(np.isinf(x_val_norm)):
+    print("INF")
+    raise ValueError
+
+if np.any(np.isnan(x_val_norm)):
+    print("NAN")
+    raise ValueError
+
+if np.any(np.isinf(x_test_norm)):
+    print("INF")
+    raise ValueError
+
+if np.any(np.isnan(x_test_norm)):
+    print("NAN")
+    raise ValueError
+
+if np.any(np.isinf(y_train_norm)):
+    print("INF")
+    raise ValueError
+
+if np.any(np.isnan(y_train_norm)):
+    print("NAN")
+    raise ValueError
+
+if np.any(np.isinf(y_val_norm)):
+    print("INF")
+    raise ValueError
+
+if np.any(np.isnan(y_val_norm)):
+    print("NAN")
+    raise ValueError
+
+if np.any(np.isinf(y_test_norm)):
+    print("INF")
+    raise ValueError
+
+if np.any(np.isnan(y_test_norm)):
+    print("NAN")
+    raise ValueError
+
+
+def get_corr_matrix(data, seq_len):
+    corr_matrices = np.zeros(shape=(data.shape[0] - seq_len, data.shape[1], data.shape[1]))
+
+    for i in tqdm(range(data.shape[0] - seq_len)):
+        data_corr = data[i:i + seq_len]
+        data_hm = pd.DataFrame(data_corr, index=range(data_corr.shape[0]),
+                               columns=['{}'.format(x + 1) for x in range(data_corr.shape[1])])
+
+        corr_matrices[i] = data_hm.corr()
+
+    corr_matrix = np.mean(corr_matrices, axis=0)
+
+    return corr_matrix
+
+adj_mx = get_corr_matrix(train_data2d, seq_len)
+
+print("|-- ADJ MX")
+print(adj_mx.shape)
+print(adj_mx.max())
+print(adj_mx.min())
+print(adj_mx.mean())
+print(adj_mx.std())
+
+
+

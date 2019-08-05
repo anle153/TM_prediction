996d053b6d9b2ddc34ed0bcdd36cdb638615e2b9
anle153 <anle@nii.ac.jp>
2019-08-05 19:28:31 +0900

    commit

--- Models/dcrnn_supervisor.py
+++ Models/dcrnn_supervisor.py
@@ -493,7 +493,7 @@ class DCRNNSupervisor(object):
             print('ER: {}'.format(er))
 
             a = np.argwhere(test_data == tm_pred)
-            print(a.sum() / a.size())
+            print(a.shape[0] / a.size())
 
         return
 

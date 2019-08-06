b77b5224a08fb0e0083e6357db670345f7566ffa
anle153 <anle@nii.ac.jp>
2019-08-06 20:45:08 +0900

    commit

--- Models/dcrnn_supervisor.py
+++ Models/dcrnn_supervisor.py
@@ -490,12 +490,12 @@ class DCRNNSupervisor(object):
             m_indicator = test_results['m_indicator']
             mape = metrics.masked_mape_np(preds=tm_pred,
                                           labels=scaler.inverse_transform(
-                                              test_data_norm[self._seq_len:-self._horizon]),
+                                              test_data_norm[self._seq_len:-(self._horizon - 1)]),
                                           null_val=0)
             print('MAPE: {}'.format(mape))
 
             er = error_ratio(y_pred=tm_pred,
-                             y_true=scaler.inverse_transform(test_data_norm[self._seq_len:-self._horizon]),
+                             y_true=scaler.inverse_transform(test_data_norm[self._seq_len:-(self._horizon - 1)]),
                              measured_matrix=m_indicator)
             print('ER: {}'.format(er))
 

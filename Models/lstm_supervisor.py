8d2b9cb19b30e382caad942c91f23f2cbbf08f7f
anle153 <anle@nii.ac.jp>
2019-08-07 17:47:30 +0900

    commit

--- Models/lstm_supervisor.py
+++ Models/lstm_supervisor.py
@@ -392,11 +392,11 @@ class lstm(AbstractModel):
                 predictions.append(y_pred)
 
                 mse = metrics.masked_mse_np(preds=y_pred, labels=y_truth, null_val=0)
-                mape = metrics.masked_mape_np(preds=y_pred, labels=y_truth, null_val=0)
+                mae = metrics.masked_mae_np(preds=y_pred, labels=y_truth, null_val=0)
                 rmse = metrics.masked_rmse_np(preds=y_pred, labels=y_truth, null_val=0)
                 self._logger.info(
-                    "Horizon {:02d}, MSE: {:.2f}, MAPE: {:.4f}, RMSE: {:.2f}".format(
-                        horizon_i + 1, mse, mape, rmse
+                    "Horizon {:02d}, MSE: {:.2f}, MAE: {:.4f}, RMSE: {:.2f}".format(
+                        horizon_i + 1, mse, mae, rmse
                     )
                 )
 

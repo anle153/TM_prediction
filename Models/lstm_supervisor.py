c0af06d51a2dd33df9aaee898135521696679fb6
anle153 <anle@nii.ac.jp>
2019-08-07 21:02:50 +0900

    commit

--- Models/lstm_supervisor.py
+++ Models/lstm_supervisor.py
@@ -393,10 +393,11 @@ class lstm(AbstractModel):
 
                 mse = metrics.masked_mse_np(preds=y_pred, labels=y_truth, null_val=0)
                 mae = metrics.masked_mae_np(preds=y_pred, labels=y_truth, null_val=0)
+                mape = metrics.masked_mape_np(preds=y_pred, labels=y_truth, null_val=0)
                 rmse = metrics.masked_rmse_np(preds=y_pred, labels=y_truth, null_val=0)
                 self._logger.info(
-                    "Horizon {:02d}, MSE: {:.2f}, MAE: {:.4f}, RMSE: {:.2f}".format(
-                        horizon_i + 1, mse, mae, rmse
+                    "Horizon {:02d}, MSE: {:.2f}, MAE: {:.2f}, RMSE: {:.2f}, MAPE: {:.4f}".format(
+                        horizon_i + 1, mse, mae, rmse, mape
                     )
                 )
 
@@ -447,12 +448,12 @@ class lstm(AbstractModel):
         y_truth = scaler.inverse_transform(self._data['y_eval'])
 
         mse = metrics.masked_mse_np(preds=y_pred, labels=y_truth, null_val=0)
-        rmse = metrics.masked_rmse_np(preds=y_pred, labels=y_truth, null_val=0)
         mae = metrics.masked_mae_np(preds=y_pred, labels=y_truth, null_val=0)
-
+        mape = metrics.masked_mape_np(preds=y_pred, labels=y_truth, null_val=0)
+        rmse = metrics.masked_rmse_np(preds=y_pred, labels=y_truth, null_val=0)
         self._logger.info(
-            "Horizon {:02d}, MSE: {:.2f}, MAE: {:.2f}, RMSE: {:.2f}".format(
-                1, mse, mae, rmse
+            "Horizon {:02d}, MSE: {:.2f}, MAE: {:.2f}, RMSE: {:.2f}, MAPE: {:.4f}".format(
+                1, mse, mae, rmse, mape
             )
         )
 

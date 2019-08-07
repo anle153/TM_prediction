af3319be2efcd7d7a9de6720359d7b5b4008b8da
anle153 <anle@nii.ac.jp>
2019-08-07 14:33:15 +0900

    commit

--- Models/dcrnn_supervisor.py
+++ Models/dcrnn_supervisor.py
@@ -516,17 +516,18 @@ class DCRNNSupervisor(object):
             predictions.append(y_pred)
 
             mse = metrics.masked_mse_np(preds=y_pred, labels=y_truth, null_val=0)
-            mape = metrics.masked_mape_np(preds=y_pred, labels=y_truth, null_val=0)
             rmse = metrics.masked_rmse_np(preds=y_pred, labels=y_truth, null_val=0)
+            mae = metrics.masked_mae_np(preds=y_pred, labels=y_truth, null_val=0)
+
             self._logger.info(
-                "Horizon {:02d}, MSE: {:.2f}, MAPE: {:.4f}, RMSE: {:.2f}".format(
-                    horizon_i + 1, mse, mape, rmse
+                "Horizon {:02d}, MSE: {:.2f}, MAE: {:.2f}, RMSE: {:.2f}".format(
+                    horizon_i + 1, mse, mae, rmse
                 )
             )
             utils.add_simple_summary(self._writer,
                                      ['%s_%d' % (item, horizon_i + 1) for item in
-                                      ['metric/rmse', 'metric/mape', 'metric/mse']],
-                                     [rmse, mape, mse],
+                                      ['metric/rmse', 'metric/mae', 'metric/mse']],
+                                     [rmse, mae, mse],
                                      global_step=global_step)
         outputs = {
             'predictions': predictions,

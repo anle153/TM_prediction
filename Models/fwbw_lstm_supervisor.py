3295f5f57927bb54f95b72ca9c5b48d29cb96dc0
anle153 <anle@nii.ac.jp>
2019-08-09 22:56:25 +0900

    commit

--- Models/fwbw_lstm_supervisor.py
+++ Models/fwbw_lstm_supervisor.py
@@ -1,19 +1,34 @@
 import os
 import time
 
+import keras.callbacks as keras_callbacks
+import numpy as np
+import pandas as pd
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.layers import LSTM, Dense, Dropout, TimeDistributed, Flatten, Input, Concatenate, Reshape, Add
 from keras.models import Model
 from keras.utils import plot_model
+from tqdm import tqdm
 
-from Models.AbstractModel import AbstractModel
+from common.error_utils import error_ratio
 from lib import metrics
 from lib import utils
 
 
-class FwbwLstmRegression(AbstractModel):
+class TimeHistory(keras_callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.times = []
+
+    def on_epoch_begin(self, batch, logs={}):
+        self.epoch_time_start = time.time()
+
+    def on_epoch_end(self, batch, logs={}):
+        self.times.append(time.time() - self.epoch_time_start)
+
+
+class FwbwLstmRegression():
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
 
         self._kwargs = kwargs
         self._data_kwargs = kwargs.get('data')
@@ -65,6 +80,37 @@ class FwbwLstmRegression(AbstractModel):
 
         # Model
         self.model = None
+
+        self.callbacks_list = []
+
+        self.checkpoints = ModelCheckpoint(
+            self._log_dir + "best_model.hdf5",
+            monitor='val_loss', verbose=1,
+            save_best_only=True,
+            mode='auto', period=1)
+        self.callbacks_list = [self.checkpoints]
+
+        self.earlystop = EarlyStopping(monitor='val_loss', patience=50,
+                                       verbose=1, mode='auto')
+        self.callbacks_list.append(self.earlystop)
+
+        self.time_callback = TimeHistory()
+        self.callbacks_list.append(self.time_callback)
+
+    def save_model_history(self, model_history):
+        loss = np.array(model_history.history['loss'])
+        val_loss = np.array(model_history.history['val_loss'])
+        dump_model_history = pd.DataFrame(index=range(loss.size),
+                                          columns=['epoch', 'loss', 'val_loss', 'train_time'])
+
+        dump_model_history['epoch'] = range(loss.size)
+        dump_model_history['loss'] = loss
+        dump_model_history['val_loss'] = val_loss
+
+        if self.time_callback.times is not None:
+            dump_model_history['train_time'] = self.time_callback.times
+
+        dump_model_history.to_csv(self._log_dir + 'training_history.csv', index=False)
 
     @staticmethod
     def _get_log_dir(kwargs):
@@ -249,6 +295,104 @@ class FwbwLstmRegression(AbstractModel):
         plt.legend()
         plt.close()
 
+    def _prepare_input(self, data, m_indicator):
+
+        dataX = np.zeros(shape=(data.shape[1], self._seq_len, self._input_dim), dtype='float32')
+        for flow_id in range(data.shape[1]):
+            x = data[:, flow_id]
+            label = m_indicator[:, flow_id]
+
+            dataX[flow_id, :, 0] = x
+            dataX[flow_id, :, 1] = label
+
+        return dataX
+
+    def _ims_tm_prediction(self, init_data, init_labels):
+        multi_steps_tm = np.zeros(shape=(init_data.shape[0] + self._horizon, init_data.shape[1]),
+                                  dtype='float32')
+        multi_steps_tm[0:self._seq_len] = init_data
+
+        m_indicator = np.zeros(shape=(init_labels.shape[0] + self._horizon, init_labels.shape[1]),
+                               dtype='float32')
+        m_indicator[0:self._seq_len] = init_labels
+
+        bw_outputs = None
+
+        for ts_ahead in range(self._horizon):
+            rnn_input = self._prepare_input(data=multi_steps_tm[ts_ahead:ts_ahead + self._seq_len],
+                                            m_indicator=m_indicator[ts_ahead:ts_ahead + self._seq_len])
+            predictX_1, predictX_2 = self.model.predict(rnn_input)
+            multi_steps_tm[ts_ahead + self._seq_len] = np.squeeze(predictX_1, axis=1)
+
+            if ts_ahead == 0:
+                bw_outputs = predictX_2
+
+        return multi_steps_tm[-self._horizon:], bw_outputs
+
+    def _run_tm_prediction(self):
+        test_data_norm = self._data['test_data_norm']
+
+        tf_a = np.array([1.0, 0.0])
+        m_indicator = np.zeros(shape=(test_data_norm.shape[0] - self._horizon, self._nodes),
+                               dtype='float32')
+
+        tm_pred = np.zeros(shape=(test_data_norm.shape[0] - self._horizon, self._nodes),
+                           dtype='float32')
+
+        tm_pred[0:self._seq_len] = test_data_norm[0:self._seq_len]
+        m_indicator[0:self._seq_len] = np.ones(shape=(self._seq_len, self._nodes))
+
+        y_preds = []
+        y_truths = []
+
+        # Predict the TM from time slot look_back
+        for ts in tqdm(range(test_data_norm.shape[0] - self._horizon - self._seq_len)):
+            # This block is used for iterated multi-step traffic matrices prediction
+
+            predicted_tm, bw_outputs = self._ims_tm_prediction(init_data=tm_pred[ts:ts + self._seq_len],
+                                                               init_labels=m_indicator[ts:ts + self._seq_len])
+
+            # Get the TM prediction of next time slot
+
+            y_preds.append(np.expand_dims(predicted_tm, axis=0))
+            pred = predicted_tm[0]
+
+            # Using part of current prediction as input to the next estimation
+            # Randomly choose the flows which is measured (using the correct data from test_set)
+
+            # boolean array(1 x n_flows):for choosing value from predicted data
+            if self._flow_selection == 'Random':
+                sampling = np.random.choice(tf_a, size=self._nodes,
+                                            p=[self._mon_ratio, 1 - self._mon_ratio])
+            else:
+                sampling = self._set_measured_flow_fairness(m_indicator=m_indicator[ts: ts + self._seq_len])
+
+            m_indicator[ts + self._seq_len] = sampling
+            # invert of sampling: for choosing value from the original data
+            inv_sampling = 1.0 - sampling
+            pred_input = pred * inv_sampling
+
+            ground_true = test_data_norm[ts + self._seq_len]
+            y_truths.append(
+                np.expand_dims(test_data_norm[ts + self._seq_len:ts + self._seq_len + self._horizon], axis=0))
+
+            measured_input = ground_true * sampling
+
+            # Merge value from pred_input and measured_input
+            new_input = pred_input + measured_input
+            # new_input = np.reshape(new_input, (new_input.shape[0], new_input.shape[1], 1))
+
+            # Concatenating new_input into current rnn_input
+            tm_pred[ts + self._seq_len] = new_input
+
+        outputs = {
+            'tm_pred': tm_pred[self._seq_len:],
+            'm_indicator': m_indicator[self._seq_len:],
+            'y_preds': y_preds,
+            'y_truths': y_truths
+        }
+        return outputs
+
     def train(self):
         training_fw_history = self.model.fit(x=self._data['x_train'],
                                              y=[self._data['y_train_1'], self._data['y_train_2']],
@@ -291,6 +435,46 @@ class FwbwLstmRegression(AbstractModel):
                 mse_2, mae_2, rmse_2, mape_2
             )
         )
+
+    def test(self):
+        scaler = self._data['scaler']
+
+        for i in range(self._run_times):
+            print('|--- Running time: {}/{}'.format(i, self._run_times))
+
+            outputs = self._run_tm_prediction()
+
+            tm_pred, m_indicator, y_preds = outputs['tm_pred'], outputs['m_indicator'], outputs['y_preds']
+
+            y_preds = np.concatenate(y_preds, axis=0)
+            predictions = []
+            y_truths = outputs['y_truths']
+            y_truths = np.concatenate(y_truths, axis=0)
+
+            for horizon_i in range(self._horizon):
+                y_truth = scaler.inverse_transform(y_truths[:, horizon_i, :])
+
+                y_pred = scaler.inverse_transform(y_preds[:, horizon_i, :])
+                predictions.append(y_pred)
+
+                mse = metrics.masked_mse_np(preds=y_pred, labels=y_truth, null_val=0)
+                mae = metrics.masked_mae_np(preds=y_pred, labels=y_truth, null_val=0)
+                mape = metrics.masked_mape_np(preds=y_pred, labels=y_truth, null_val=0)
+                rmse = metrics.masked_rmse_np(preds=y_pred, labels=y_truth, null_val=0)
+                self._logger.info(
+                    "Horizon {:02d}, MSE: {:.2f}, MAE: {:.2f}, RMSE: {:.2f}, MAPE: {:.4f}".format(
+                        horizon_i + 1, mse, mae, rmse, mape
+                    )
+                )
+
+            tm_pred = scaler.inverse_transform(tm_pred)
+
+            er = error_ratio(y_pred=tm_pred,
+                             y_true=scaler.inverse_transform(
+                                 self._data['test_data_norm'][self._seq_len:-self._horizon]),
+                             measured_matrix=m_indicator)
+            print('ER: {}'.format(er))
+
 
     def load(self):
         self.model.load_weights(self.saving_path+'best_model.hdf5')

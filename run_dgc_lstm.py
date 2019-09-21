34ca12903f9890890afc32fc66a116a04ee1bbdd
anle <anle@nii.ac.jp>
2019-09-21 12:41:25 +0900

    commit

--- run_dgc_lstm.py
+++ run_dgc_lstm.py
@@ -89,7 +89,7 @@ if __name__ == '__main__':
     sys.path.append(os.getcwd())
     parser = argparse.ArgumentParser()
     parser.add_argument('--use_cpu_only', default=False, type=str, help='Whether to run tensorflow on cpu.')
-    parser.add_argument('--config_file', default='data/model/pretrained/METR-LA/config.yaml', type=str,
+    parser.add_argument('--config-file', default='data/model/pretrained/METR-LA/config.yaml', type=str,
                         help='Config file for pretrained model.')
     parser.add_argument('--mode', default='train', type=str,
                         help='Run mode.')

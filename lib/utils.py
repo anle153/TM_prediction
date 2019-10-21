b1a05fb5a93467a66eb73ebfeb992765518e823c
anle153 <anle@nii.ac.jp>
2019-10-21 22:03:02 +0900

    commit

--- lib/utils.py
+++ lib/utils.py
@@ -285,7 +285,7 @@ def create_data_dcrnn_weighted(data, seq_len, horizon, input_dim, mon_ratio, eps
 
         _w = np.zeros(shape=(seq_len, _data.shape[1], 1))
         for i in range(seq_len):
-            _mr = float(_labels[(idx + i):(idx + seq_len + i)].sum(axis=0)) / seq_len
+            _mr = _labels[(idx + i):(idx + seq_len + i)].sum(axis=0) / seq_len
             _w[i] = np.expand_dims(_mr, axis=1)
 
         # x[idx] = np.stack([_x, _label, _w], axis=2)

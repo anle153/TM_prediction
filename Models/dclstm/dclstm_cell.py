23a24368d117b780d280d395dab20b1ee51dbf9a
anle153 <anle@nii.ac.jp>
2019-11-16 22:30:27 +0900

    dclstm

---
+++ Models/dclstm/dclstm_cell.py
@@ -0,0 +1,190 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib.rnn import RNNCell
+
+from lib import utils
+
+
+class DCLSTMCell(RNNCell):
+    """Graph Convolution Gated Recurrent Unit cell.
+    """
+
+    def call(self, inputs, **kwargs):
+        pass
+
+    def compute_output_shape(self, input_shape):
+        pass
+
+    def __init__(self, num_units, adj_mx, max_diffusion_step, num_nodes, num_proj=None,
+                 activation=tf.nn.tanh, reuse=None, filter_type="laplacian", use_gc_for_ru=True):
+        """
+
+        :param num_units:
+        :param adj_mx:
+        :param max_diffusion_step:
+        :param num_nodes:
+        :param input_size:
+        :param num_proj:
+        :param activation:
+        :param reuse:
+        :param filter_type: "laplacian", "random_walk", "dual_random_walk".
+        :param use_gc_for_ru: whether to use Graph convolution to calculate the reset and update gates.
+        """
+        super(DCLSTMCell, self).__init__(_reuse=reuse)
+        self._activation = activation
+        self._num_nodes = num_nodes
+        self._num_proj = num_proj
+        self._num_units = num_units
+        self._max_diffusion_step = max_diffusion_step
+        self._supports = []
+        self._use_gc_for_ru = use_gc_for_ru
+        supports = []
+        if filter_type == "laplacian":
+            supports.append(utils.calculate_scaled_laplacian(adj_mx, lambda_max=None))
+        elif filter_type == "random_walk":
+            supports.append(utils.calculate_random_walk_matrix(adj_mx).T)
+        elif filter_type == "dual_random_walk":
+            supports.append(utils.calculate_random_walk_matrix(adj_mx).T)
+            supports.append(utils.calculate_random_walk_matrix(adj_mx.T).T)
+        else:
+            supports.append(utils.calculate_scaled_laplacian(adj_mx))
+        for support in supports:
+            self._supports.append(self._build_sparse_matrix(support))
+
+    @staticmethod
+    def _build_sparse_matrix(L):
+        L = L.tocoo()
+        indices = np.column_stack((L.row, L.col))
+        L = tf.SparseTensor(indices, L.data, L.shape)
+        return tf.sparse_reorder(L)
+
+    @property
+    def state_size(self):
+        return self._num_nodes * self._num_units
+
+    @property
+    def output_size(self):
+        output_size = self._num_nodes * self._num_units
+        if self._num_proj is not None:
+            output_size = self._num_nodes * self._num_proj
+        return output_size
+
+    def __call__(self, inputs, state, scope=None):
+        """Gated recurrent unit (GRU) with Graph Convolution.
+        :param inputs: (B, num_nodes * input_dim)
+
+        :return
+        - Output: A `2-D` tensor with shape `[batch_size x self.output_size]`.
+        - New state: Either a single `2-D` tensor, or a tuple of tensors matching
+            the arity and shapes of `state`
+        """
+        with tf.variable_scope(scope or "dclstm_cell"):
+            with tf.variable_scope("gates"):  # Reset gate and update gate.
+                output_size = 2 * self._num_units
+                # We start with bias of 1.0 to not reset and not update.
+                if self._use_gc_for_ru:
+                    fn = self._gconv
+                else:
+                    fn = self._fc
+
+                h, c = tf.split(value=state, num_or_size_splits=2, axis=-1)
+                value = tf.nn.sigmoid(fn(inputs, h, output_size, bias_start=1.0))
+                value = tf.reshape(value, (-1, self._num_nodes, output_size))
+                f_t, i_t = tf.split(value=value, num_or_size_splits=2, axis=-1)
+                f_t = tf.reshape(f_t, (-1, self._num_nodes * self._num_units))
+                i_t = tf.reshape(i_t, (-1, self._num_nodes * self._num_units))
+            with tf.variable_scope("state"):
+                _c = self._gconv(inputs, state, self._num_units)
+                if self._activation is not None:
+                    _c = self._activation(_c)
+                c = f_t * c + i_t * _c
+            output = self._gconv(inputs, h, self._num_units)
+            h = output * self._activation(c)
+
+            state = tf.concat([h, c], axis=-1)
+
+            if self._num_proj is not None:
+                with tf.variable_scope("projection"):
+                    w = tf.get_variable('w', shape=(self._num_units, self._num_proj))
+                    batch_size = inputs.get_shape()[0].value
+                    output = tf.reshape(output, shape=(-1, self._num_units))
+                    output = tf.reshape(tf.matmul(output, w), shape=(batch_size, self.output_size))
+        return output, state
+
+    @staticmethod
+    def _concat(x, x_):
+        x_ = tf.expand_dims(x_, 0)
+        return tf.concat([x, x_], axis=0)
+
+    def _fc(self, inputs, state, output_size, bias_start=0.0):
+        dtype = inputs.dtype
+        batch_size = inputs.get_shape()[0].value
+        inputs = tf.reshape(inputs, (batch_size * self._num_nodes, -1))
+        state = tf.reshape(state, (batch_size * self._num_nodes, -1))
+        inputs_and_state = tf.concat([inputs, state], axis=-1)
+        input_size = inputs_and_state.get_shape()[-1].value
+        weights = tf.get_variable(
+            'weights', [input_size, output_size], dtype=dtype,
+            initializer=tf.contrib.layers.xavier_initializer())
+        value = tf.nn.sigmoid(tf.matmul(inputs_and_state, weights))
+        biases = tf.get_variable("biases", [output_size], dtype=dtype,
+                                 initializer=tf.constant_initializer(bias_start, dtype=dtype))
+        value = tf.nn.bias_add(value, biases)
+        return value
+
+    def _gconv(self, inputs, state, output_size, bias_start=0.0):
+        """Graph convolution between input and the graph matrix.
+
+        :param args: a 2D Tensor or a list of 2D, batch x n, Tensors.
+        :param output_size:
+        :param bias:
+        :param bias_start:
+        :param scope:
+        :return:
+        """
+        # Reshape input and state to (batch_size, num_nodes, input_dim/state_dim)
+        batch_size = inputs.get_shape()[0].value
+        inputs = tf.reshape(inputs, (batch_size, self._num_nodes, -1))
+        state = tf.reshape(state, (batch_size, self._num_nodes, -1))
+        inputs_and_state = tf.concat([inputs, state], axis=2)
+        input_size = inputs_and_state.get_shape()[2].value
+        dtype = inputs.dtype
+
+        x = inputs_and_state
+        x0 = tf.transpose(x, perm=[1, 2, 0])  # (num_nodes, total_arg_size, batch_size)
+        x0 = tf.reshape(x0, shape=[self._num_nodes, input_size * batch_size])
+        x = tf.expand_dims(x0, axis=0)
+
+        scope = tf.get_variable_scope()
+        with tf.variable_scope(scope):
+            if self._max_diffusion_step == 0:
+                pass
+            else:
+                for support in self._supports:
+                    x1 = tf.sparse_tensor_dense_matmul(support, x0)
+                    x = self._concat(x, x1)
+
+                    for k in range(2, self._max_diffusion_step + 1):
+                        x2 = 2 * tf.sparse_tensor_dense_matmul(support, x1) - x0
+                        x = self._concat(x, x2)
+                        x1, x0 = x2, x1
+
+            num_matrices = len(self._supports) * self._max_diffusion_step + 1  # Adds for x itself.
+            x = tf.reshape(x, shape=[num_matrices, self._num_nodes, input_size, batch_size])
+            x = tf.transpose(x, perm=[3, 1, 2, 0])  # (batch_size, num_nodes, input_size, order)
+            x = tf.reshape(x, shape=[batch_size * self._num_nodes, input_size * num_matrices])
+
+            weights = tf.get_variable(
+                'weights', [input_size * num_matrices, output_size], dtype=dtype,
+                initializer=tf.contrib.layers.xavier_initializer())
+            x = tf.matmul(x, weights)  # (batch_size * self._num_nodes, output_size)
+
+            biases = tf.get_variable("biases", [output_size], dtype=dtype,
+                                     initializer=tf.constant_initializer(bias_start, dtype=dtype))
+            x = tf.nn.bias_add(x, biases)
+        # Reshape res back to 2D: (batch_size, num_node, state_dim) -> (batch_size, num_node * state_dim)
+        return tf.reshape(x, [batch_size, self._num_nodes * output_size])
---
+++ Models/dclstm/dclstm_supervisor.py
@@ -0,0 +1,406 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import sys
+import time
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import yaml
+from tqdm import tqdm
+
+from Models.AbstractModel import AbstractModel
+from Models.dclstm.dclstm_model import DCLSTMModel
+from lib import utils, metrics
+from lib.AMSGrad import AMSGrad
+from lib.metrics import masked_mse_loss
+
+
+class DCLSTMSupervisor(AbstractModel):
+    """
+    Do experiments using Graph Random Walk RNN model.
+    """
+
+    def __init__(self, is_training=False, **kwargs):
+        super(DCLSTMSupervisor, self).__init__(**kwargs)
+
+        self._data = utils.load_dataset_dcrnn(seq_len=self._model_kwargs.get('seq_len'),
+                                              horizon=self._model_kwargs.get('horizon'),
+                                              input_dim=self._model_kwargs.get('input_dim'),
+                                              mon_ratio=self._mon_ratio,
+                                              scaler_type=self._kwargs.get('scaler'),
+                                              is_training=is_training,
+                                              **self._data_kwargs)
+        for k, v in self._data.items():
+            if hasattr(v, 'shape'):
+                self._logger.info((k, v.shape))
+
+        # Build models.
+        scaler = self._data['scaler']
+        with tf.name_scope('Train'):
+            with tf.variable_scope('DCRNN', reuse=False):
+                self._train_model = DCLSTMModel(is_training=True, scaler=scaler,
+                                                batch_size=self._data_kwargs['batch_size'],
+                                                adj_mx=self._data['adj_mx'], **self._model_kwargs)
+
+        with tf.name_scope('Val'):
+            with tf.variable_scope('DCRNN', reuse=True):
+                self._val_model = DCLSTMModel(is_training=False, scaler=scaler,
+                                              batch_size=self._data_kwargs['val_batch_size'],
+                                              adj_mx=self._data['adj_mx'], **self._model_kwargs)
+
+        with tf.name_scope('Eval'):
+            with tf.variable_scope('DCRNN', reuse=True):
+                self._eval_model = DCLSTMModel(is_training=False, scaler=scaler,
+                                               batch_size=self._data_kwargs['eval_batch_size'],
+                                               adj_mx=self._data['adj_mx'], **self._model_kwargs)
+
+        with tf.name_scope('Test'):
+            with tf.variable_scope('DCRNN', reuse=True):
+                self._test_model = DCLSTMModel(is_training=False, scaler=scaler,
+                                               batch_size=self._data_kwargs['test_batch_size'],
+                                               adj_mx=self._data['adj_mx'], **self._model_kwargs)
+
+        # Learning rate.
+        self._lr = tf.get_variable('learning_rate', shape=(), initializer=tf.constant_initializer(0.01),
+                                   trainable=False)
+        self._new_lr = tf.placeholder(tf.float32, shape=(), name='new_learning_rate')
+        self._lr_update = tf.assign(self._lr, self._new_lr, name='lr_update')
+
+        # Configure optimizer
+        optimizer_name = self._train_kwargs.get('optimizer', 'adam').lower()
+        epsilon = float(self._train_kwargs.get('epsilon', 1e-3))
+        optimizer = tf.train.AdamOptimizer(self._lr, epsilon=epsilon, )
+        if optimizer_name == 'sgd':
+            optimizer = tf.train.GradientDescentOptimizer(self._lr, )
+        elif optimizer_name == 'amsgrad':
+            optimizer = AMSGrad(self._lr, epsilon=epsilon)
+
+        # Calculate loss
+        output_dim = self._model_kwargs.get('output_dim')
+        preds = self._train_model.outputs
+        labels = self._train_model.labels[..., :output_dim]
+
+        null_val = 0.
+        self._loss_fn = masked_mse_loss(scaler, null_val)
+        # self._loss_fn = masked_mae_loss(scaler, null_val)
+        self._train_loss = self._loss_fn(preds=preds, labels=labels)
+
+        tvars = tf.trainable_variables()
+        grads = tf.gradients(self._train_loss, tvars)
+        max_grad_norm = kwargs['train'].get('max_grad_norm', 1.)
+        grads, _ = tf.clip_by_global_norm(grads, max_grad_norm)
+        global_step = tf.train.get_or_create_global_step()
+        self._train_op = optimizer.apply_gradients(zip(grads, tvars), global_step=global_step, name='train_op')
+
+        max_to_keep = self._train_kwargs.get('max_to_keep', 100)
+        self._epoch = 0
+        self._saver = tf.train.Saver(tf.global_variables(), max_to_keep=max_to_keep)
+
+        # Log model statistics.
+        total_trainable_parameter = utils.get_total_trainable_parameter_size()
+        self._logger.info('Total number of trainable parameters: {:d}'.format(total_trainable_parameter))
+        for var in tf.global_variables():
+            self._logger.debug('{}, {}'.format(var.name, var.get_shape()))
+
+    def run_epoch_generator(self, sess, model, data_generator, return_output=False, training=False, writer=None):
+        losses = []
+        mses = []
+        outputs = []
+        output_dim = self._model_kwargs.get('output_dim')
+        preds = model.outputs
+        labels = model.labels[..., :output_dim]
+        loss = self._loss_fn(preds=preds, labels=labels)
+        fetches = {
+            'loss': loss,
+            'mse': loss,
+            'global_step': tf.train.get_or_create_global_step()
+        }
+        if training:
+            fetches.update({
+                'train_op': self._train_op
+            })
+            merged = model.merged
+            if merged is not None:
+                fetches.update({'merged': merged})
+
+        if return_output:
+            fetches.update({
+                'outputs': model.outputs
+            })
+
+        for _, (x, y) in enumerate(data_generator):
+
+            feed_dict = {
+                model.inputs: x,
+                model.labels: y,
+            }
+
+            vals = sess.run(fetches, feed_dict=feed_dict)
+
+            losses.append(vals['loss'])
+            mses.append(vals['mse'])
+            if writer is not None and 'merged' in vals:
+                writer.add_summary(vals['merged'], global_step=vals['global_step'])
+            if return_output:
+                outputs.append(vals['outputs'])
+
+        results = {
+            'loss': np.mean(losses),
+            'mse': np.mean(mses)
+        }
+        if return_output:
+            results['outputs'] = outputs
+        return results
+
+    def _prepare_input(self, ground_truth, data, m_indicator):
+
+        x = np.zeros(shape=(self._seq_len, self._nodes, self._input_dim), dtype='float32')
+        y = np.zeros(shape=(self._horizon, self._nodes), dtype='float32')
+
+        x[:, :, 0] = data
+        x[:, :, 1] = m_indicator
+
+        y[:] = ground_truth
+        y = np.expand_dims(y, axis=2)
+
+        return np.expand_dims(x, axis=0), np.expand_dims(y, axis=0)
+
+    def _run_tm_prediction(self, sess, model, runId, writer=None):
+
+        test_data_norm = self._data['test_data_norm']
+
+        # Initialize traffic matrix data
+        tm_pred, m_indicator = self._init_data_test(test_data_norm, runId)
+
+        y_preds = []
+        fetches = {
+            'global_step': tf.train.get_or_create_global_step()
+        }
+
+        fetches.update({
+            'outputs': model.outputs
+        })
+
+        y_truths = []
+
+        for ts in tqdm(range(test_data_norm.shape[0] - self._horizon - self._seq_len)):
+
+            x = self._prepare_input_dcrnn(
+                data=tm_pred[ts:ts + self._seq_len],
+                m_indicator=m_indicator[ts:ts + self._seq_len]
+            )
+
+            y_truths.append(
+                np.expand_dims(test_data_norm[ts + self._seq_len:ts + self._seq_len + self._horizon].copy(), axis=0))
+
+            feed_dict = {
+                model.inputs: x,
+            }
+
+            vals = sess.run(fetches, feed_dict=feed_dict)
+            y_preds.append(np.squeeze(vals['outputs'], axis=-1))
+
+            if writer is not None and 'merged' in vals:
+                writer.add_summary(vals['merged'], global_step=vals['global_step'])
+
+            pred = vals['outputs'][0, 0, :, 0]
+
+            sampling = self._monitored_flows_slection(time_slot=ts, m_indicator=m_indicator)
+
+            # invert of sampling: for choosing value from the original data
+
+            ground_true = test_data_norm[ts + self._seq_len]
+
+            # Merge value from pred_input and measured_input
+            new_input = pred * (1.0 - sampling) + ground_true * sampling
+
+            # Concatenating new_input into current rnn_input
+            tm_pred[ts + self._seq_len] = new_input
+
+        results = {'y_preds': y_preds,
+                   'tm_pred': tm_pred[self._seq_len:],
+                   'm_indicator': m_indicator[self._seq_len:],
+                   'y_truths': y_truths
+                   }
+        return results
+
+    def get_lr(self, sess):
+        return sess.run(self._lr).item()
+
+    def set_lr(self, sess, lr):
+        sess.run(self._lr_update, feed_dict={
+            self._new_lr: lr
+        })
+
+    def _train(self, sess, base_lr, epoch, steps, patience=50, epochs=100,
+               min_learning_rate=2e-6, lr_decay_ratio=0.1, save_model=1,
+               test_every_n_epochs=10, **train_kwargs):
+        history = []
+        min_val_loss = float('inf')
+        wait = 0
+
+        training_history = pd.DataFrame()
+        losses, val_losses = [], []
+
+        max_to_keep = train_kwargs.get('max_to_keep', 100)
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=max_to_keep)
+        model_filename = train_kwargs.get('model_filename')
+        continue_train = train_kwargs.get('continue_train')
+        if continue_train is True and model_filename is not None:
+            saver.restore(sess, model_filename)
+            self._epoch = epoch + 1
+        else:
+            sess.run(tf.global_variables_initializer())
+        self._logger.info('Start training ...')
+
+        while self._epoch <= epochs:
+            self._logger.info('Training epoch: {}/{}'.format(self._epoch, epochs))
+            # Learning rate schedule.
+            new_lr = max(min_learning_rate, base_lr * (lr_decay_ratio ** np.sum(self._epoch >= np.array(steps))))
+            self.set_lr(sess=sess, lr=new_lr)
+
+            start_time = time.time()
+            train_results = self.run_epoch_generator(sess, self._train_model,
+                                                     self._data['train_loader'].get_iterator(),
+                                                     training=True,
+                                                     writer=self._writer)
+            train_loss, train_mse = train_results['loss'], train_results['mse']
+            # if train_loss > 1e5:
+            #     self._logger.warning('Gradient explosion detected. Ending...')
+            #     break
+
+            global_step = sess.run(tf.train.get_or_create_global_step())
+            # Compute validation error.
+            val_results = self.run_epoch_generator(sess, self._val_model,
+                                                   self._data['val_loader'].get_iterator(),
+                                                   training=False)
+            val_loss, val_mse = val_results['loss'].item(), val_results['mse'].item()
+
+            utils.add_simple_summary(self._writer,
+                                     ['loss/train_loss', 'metric/train_mse', 'loss/val_loss', 'metric/val_mse'],
+                                     [train_loss, train_mse, val_loss, val_mse], global_step=global_step)
+            end_time = time.time()
+            message = 'Epoch [{}/{}] ({}) train_mse: {:f}, val_mse: {:f} lr:{:f} {:.1f}s'.format(
+                self._epoch, epochs, global_step, train_mse, val_mse, new_lr, (end_time - start_time))
+            self._logger.info(message)
+            if self._epoch % test_every_n_epochs == test_every_n_epochs - 1:
+                self.evaluate(sess)
+            if val_loss <= min_val_loss:
+                wait = 0
+                if save_model > 0:
+                    model_filename = self.save(sess, val_loss)
+                self._logger.info(
+                    'Val loss decrease from %f to %f, saving to %s' % (min_val_loss, val_loss, model_filename))
+                min_val_loss = val_loss
+            else:
+                wait += 1
+                if wait > patience:
+                    self._logger.warning('Early stopping at epoch: %d' % self._epoch)
+                    break
+
+            history.append(val_mse)
+            # Increases epoch.
+            self._epoch += 1
+            losses.append(train_loss)
+            val_losses.append(val_loss)
+            sys.stdout.flush()
+
+        training_history['epoch'] = np.arange(self._epoch)
+        training_history['loss'] = losses
+        training_history['val_loss'] = val_losses
+        training_history.to_csv(self._log_dir + 'training_history.csv', index=False)
+
+        return np.min(history)
+
+    def test(self, sess):
+
+        n_metrics = 4
+        # Metrics: MSE, MAE, RMSE, MAPE, ER
+        metrics_summary = np.zeros(shape=(self._run_times + 3, self._horizon * n_metrics + 1))
+
+        for i in range(self._run_times):
+            self._logger.info('|--- Run time: {}'.format(i))
+            # y_test = self._prepare_test_set()
+
+            test_results = self._run_tm_prediction(sess, model=self._test_model, runId=i)
+
+            metrics_summary = self._calculate_metrics(prediction_results=test_results, metrics_summary=metrics_summary,
+                                                      scaler=self._data['scaler'],
+                                                      runId=i, data_norm=self._data['test_data_norm'])
+
+        self._summarize_results(metrics_summary=metrics_summary, n_metrics=n_metrics)
+
+        return
+
+    def train(self, sess, **kwargs):
+        kwargs.update(self._train_kwargs)
+        return self._train(sess, **kwargs)
+
+    def evaluate(self, sess):
+        global_step = sess.run(tf.train.get_or_create_global_step())
+        test_results = self.run_epoch_generator(sess, self._eval_model,
+                                                self._data['eval_loader'].get_iterator(),
+                                                return_output=True,
+                                                training=False)
+
+        # y_preds:  a list of (batch_size, horizon, num_nodes, output_dim)
+        test_loss, y_preds = test_results['loss'], test_results['outputs']
+        utils.add_simple_summary(self._writer, ['loss/test_loss'], [test_loss], global_step=global_step)
+
+        y_preds = np.concatenate(y_preds, axis=0)
+        scaler = self._data['scaler']
+        predictions = []
+        y_truths = []
+        for horizon_i in range(self._data['y_eval'].shape[1]):
+            y_truth = scaler.inverse_transform(self._data['y_eval'][:, horizon_i, :, 0])
+            y_truths.append(y_truth)
+
+            y_pred = scaler.inverse_transform(y_preds[:, horizon_i, :, 0])
+            predictions.append(y_pred)
+
+            mse = metrics.masked_mse_np(preds=y_pred, labels=y_truth, null_val=0)
+            mae = metrics.masked_mae_np(preds=y_pred, labels=y_truth, null_val=0)
+            mape = metrics.masked_mape_np(preds=y_pred, labels=y_truth, null_val=0)
+            rmse = metrics.masked_rmse_np(preds=y_pred, labels=y_truth, null_val=0)
+            self._logger.info(
+                "Horizon {:02d}, MSE: {:.2f}, MAE: {:.2f}, RMSE: {:.2f}, MAPE: {:.4f}".format(
+                    horizon_i + 1, mse, mae, rmse, mape
+                )
+            )
+            utils.add_simple_summary(self._writer,
+                                     ['%s_%d' % (item, horizon_i + 1) for item in
+                                      ['metric/rmse', 'metric/mae', 'metric/mse']],
+                                     [rmse, mae, mse],
+                                     global_step=global_step)
+        outputs = {
+            'predictions': predictions,
+            'groundtruth': y_truths
+        }
+        return outputs
+
+    def load(self, sess, model_filename):
+        """
+        Restore from saved model.
+        :param sess:
+        :param model_filename:
+        :return:
+        """
+        self._saver.restore(sess, model_filename)
+
+    def save(self, sess, val_loss):
+        config = dict(self._kwargs)
+        global_step = sess.run(tf.train.get_or_create_global_step()).item()
+        prefix = os.path.join(self._log_dir, 'models-{:.4f}'.format(val_loss))
+        config['train']['epoch'] = self._epoch
+        config['train']['global_step'] = global_step
+        config['train']['log_dir'] = self._log_dir
+        config['train']['model_filename'] = self._saver.save(sess, prefix, global_step=global_step,
+                                                             write_meta_graph=False)
+        config_filename = 'config_{}.yaml'.format(self._epoch)
+        with open(os.path.join(self._log_dir, config_filename), 'w') as f:
+            yaml.dump(config, f, default_flow_style=False)
+        return config['train']['model_filename']
